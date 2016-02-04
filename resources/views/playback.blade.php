6c8483693577ed1fd1cf6a7fcb002ee0e68f1c58
Wilson Omokoro <wilson.omokoro@andela.com>
2016-02-04 11:41:00 +0100

    (Chore) Allo fullscreen during playback

--- resources/views/playback.blade.php
+++ resources/views/playback.blade.php
@@ -4,15 +4,15 @@
 @section('content')
     <div class="container-fluid" style="background-color: #fff; font-family: arial;">
         <div class="row">
-            <div class="col-sm-12">
-                <h4>Playback - {{ $video->title }}</h4>
+            <div class="col-md-6">
+                <h3>{{ $video->title }}</h3>
                  <?php
                 $youtubeURL = $video->url;
                 $extracted = strrchr($youtubeURL, '=');
                 $youtubeVideoID = str_replace('=', '', $extracted);
                 ?>
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="http://www.youtube.com/embed/{{ $youtubeVideoID }}"></iframe>
+                    <iframe class="embed-responsive-item" src="http://www.youtube.com/embed/{{ $youtubeVideoID }}" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
