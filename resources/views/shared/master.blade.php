90436e3d75cda197f12c70c4b0f197d8d74ae256
Wilson Omokoro <wilson.omokoro@andela.com>
2016-02-08 22:54:51 +0100

    (Chore) Logo to navbar

--- resources/views/shared/master.blade.php
+++ resources/views/shared/master.blade.php
@@ -23,7 +23,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Brand</a>
+                <a class="navbar-brand" href="/" style="margin-top: -5px; margin-right: 55px;"><img src="/images/logo.png" /></a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
