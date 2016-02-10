30502aebc5a5425ec384fe5662197b6bb34d5d62
Wilson Omokoro <wilson.omokoro@andela.com>
2016-02-11 00:01:58 +0100

    (Fix) create 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('shared.master')
+@section('title', 'Home')
+
+@section('content')
+    <div class="container-fluid content">
+        <div class="row">
+            <div class="col-sm-12">
+                <br /><br />
+                <div class="alert alert-info" role="alert">
+                    <p><b>Hey!</b> The page you are looking for does not exist.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
