93de349a328d44bcee692c718c3cceeb75295f96
Wilson Omokoro <wilson.omokoro@andela.com>
2016-02-10 16:45:32 +0100

    (Chore) Create user profile update test

--- tests/ProfileUpdateTest.php
+++ tests/ProfileUpdateTest.php
@@ -6,13 +6,19 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ProfileUpdateTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testProfileUpdate()
     {
-        $this->assertTrue(true);
+        $user = factory(\App\User::class)->create();
+        $this->actingAs($user)
+             ->visit('/dashboard');
+
+        $this->click('My Profile');
+
+        $this->type('testuser', 'username')
+            ->type('test@domain.com', 'email')
+            ->type('Test', 'first_name')
+            ->type('User', 'last_name')
+            ->press('Save Changes')
+            ->see('You have successfully updated your profile.');
     }
 }
